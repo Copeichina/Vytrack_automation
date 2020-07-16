86bf485326b81b480dc17a563e57da1d5ff387ee
Copeichina <olga.greencard@gmail.com>
2020-07-16 15:34:26 -0400

    added new method in CRM_UI_02 branch

---
+++ src/test/java/com/vytrack/pages/SourceTree.java
@@ -0,0 +1,10 @@
+package com.vytrack.pages;
+
+public class SourceTree {
+    public static void main(String[] args) {
+        System.out.println("From intelliJ");
+    }
+    public static void newMethod(){
+        System.out.println("new method from local"+"src\\test\\java\\com\\vytrack\\pages\\SourceTree.java");
+    }
+}
